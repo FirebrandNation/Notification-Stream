7b8f36837a886f58b3e65fd02f0d0b5bb452cb32
Daggie Blanqx <daggieblanqx@gmail.com>
2018-12-23 00:00:51 +0300

    first commit!

--- scripts/git-push.js
+++ scripts/git-push.js
@@ -5,5 +5,4 @@ var simpleGit = require('simple-git');
 simpleGit()
      .add('./*')
      .commit("first commit!")
-     .addRemote('origin', 'https://github.com/DaggieBlanqx/Notification-Stream.git')
      .push(['-u', 'origin', 'master'], () => console.log('done'));
